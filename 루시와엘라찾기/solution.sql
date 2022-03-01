0ce08cc1c2727b4677983cab29ceac64ecbba797
ogdoyun <dhr8820@gmail.com>
2022-03-01 13:59:04 +0900

    refactor(루시와엘라찾기): where in 문 처리 #7

--- 루시와엘라찾기/solution.sql
+++ 루시와엘라찾기/solution.sql
@@ -1,9 +1,4 @@
 select ANIMAL_ID, NAME, SEX_UPON_INTAKE
 from ANIMAL_INS
-where NAME = 'Lucy'or 
-NAME = 'Ella' or 
-NAME = 'Pickle' 
-or NAME = 'Rogan' 
-or NAME = 'Sabrina' 
-or NAME = 'Mitty'
-order by ANIMAL_ID asc
+where NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+order by ANIMAL_ID asc;
